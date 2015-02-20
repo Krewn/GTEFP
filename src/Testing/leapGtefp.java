e14f7fa3ff3a94d6acf7551c36c30868a9ff29d8
kevin <kpie314@gmail.com>
2015-02-20 02:25:21 -0500

    leap

---
+++ src/Testing/leapGtefp.java
@@ -0,0 +1,24 @@
+package Testing;
+
+import gtefpMain.GpFrame;
+import leap.Sample;
+
+import org.junit.Test;
+import junit.framework.TestCase;
+import static org.junit.Assert.*;
+
+public class leapGtefp {
+	@Test
+	public void leap(){
+		GpFrame F1 = new GpFrame ("Gtefp");
+		boolean b = true;
+		try {
+			Sample S1 = new Sample();
+        } catch (Exception e) {
+            e.printStackTrace();
+          	b = false;
+        }
+		assertEquals(b, true);
+	}
+	
+}
---
+++ src/leap/Sample.java
@@ -0,0 +1,34 @@
+package leap;
+/******************************************************************************\
+* Copyright (C) 2012-2013 Leap Motion, Inc. All rights reserved.               *
+* Leap Motion proprietary and confidential. Not for distribution.              *
+* Use subject to the terms of the Leap Motion SDK Agreement available at       *
+* https://developer.leapmotion.com/sdk_agreement, or another agreement         *
+* between Leap Motion and you, your company or other organization.             *
+\******************************************************************************/
+
+import java.io.IOException;
+import com.leapmotion.leap.*;
+
+public class Sample {
+    public static void main(String[] args) {
+        // Create a sample listener and controller
+        SampleListener listener = new SampleListener();
+        Controller controller = new Controller();
+
+        // Have the sample listener receive events from the controller
+        controller.addListener(listener);
+
+        // Keep this process running until Enter is pressed
+        System.out.println("Press Enter to quit...");
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Remove the sample listener when done
+        controller.removeListener(listener);
+    }
+}
+
