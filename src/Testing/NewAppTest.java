cfe6243cde3aafbaa9b7f53dd373cd66c54b3e81
Owner <Owner@50.28.136.225>
2015-05-01 00:37:57 -0700

    New App Test

---
+++ src/Testing/NewAppTest.java
@@ -0,0 +1,33 @@
+package Testing;
+
+import static org.junit.Assert.*;
+import gtefpMain.GpFrame;
+import gtefpMain.WorkspacePanel;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+import util.App;
+
+public class NewAppTest extends TestCase{
+	
+	private WorkspacePanel _wp;
+	private GpFrame _gpf;
+	private boolean _testSave;
+	
+	@Test
+	
+	public void testNewApp(){
+		_wp = new WorkspacePanel(_gpf);
+		App newApp = new App(_wp);
+		assertNotNull(newApp);		//if new app is created, test will pass
+	}
+	
+	
+	public void testSaveToFile(){
+		_wp = new WorkspacePanel(_gpf);
+		App newApp = new App(_wp);
+		_testSave = newApp.saveToFile();
+		assertEquals(_testSave, false);
+	}
+}
